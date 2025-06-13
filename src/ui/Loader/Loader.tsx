681d3e51ca20002732f68a49aefa39a1dd33d862
Raxim <rahtag165@gmail.com>
2025-06-13 11:06:47 +0300

    first commit

---
+++ src/ui/Loader/Loader.tsx
@@ -0,0 +1,8 @@
+import './loader.css'
+export const Loader = () => {
+  return (
+    <div className="preloder">
+      <div className="loader"><div></div><div></div><div></div></div>
+    </div>
+  )
+}
